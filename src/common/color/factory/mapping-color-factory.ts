de69283668771aea3ca995ec75e73d44dd39aad0
blwatkins <blwatkins@users.noreply.github.com>
2023-12-29 21:06:13 -0600

    #22 Add initial range class and color factory interfaces

---
+++ src/common/color/factory/mapping-color-factory.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 Brittni Watkins.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Project Template,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+import ColorFactory from "./color-factory";
+import Color from "../color";
+
+interface MappingColorFactory extends ColorFactory {
+    mapColor(value: number, minValue: number, maxValue: number, saturation: number, brightness: number): Color;
+}
+
+export default MappingColorFactory;
